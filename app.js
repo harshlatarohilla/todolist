5908e29f019ce91c9d80b5312febd238d7012e72
Harshlata <harshlatarohilla.193@gmail.com>
2023-06-08 17:39:46 +0530

    connect backend

--- app.js
+++ app.js
@@ -1,51 +1,106 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-
-let items = ["water", "air"];
-let item = "";
+const mongoose = require("mongoose");
+const _ = require("lodash");
+const PORT = process.env.PORT || 3001;
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 app.set("view engine", "ejs");
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  let today = new Date();
-  const options = {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-  };
-  let day = today.toLocaleDateString("en-US", options);
-  let year = today.getFullYear();
+const itemSchema = new mongoose.Schema({ name: String });
+const listSchema = new mongoose.Schema({ name: String, items: [itemSchema] });
 
-  res.render("list", {
-    kindOfDay: day,
-    newListItems: items,
-    items: items,
-    year: year,
+const Item = mongoose.model("Item", itemSchema);
+const List = mongoose.model("List", listSchema);
+
+const item1 = new Item({ name: "Water" });
+const item2 = new Item({ name: "Washer" });
+const item3 = new Item({ name: "Air" });
+const defaultItems = [item1, item2, item3];
+
+app.get("/", (req, res) => {
+  Item.find({}).then((result) => {
+    //  if(result.length==0){
+    if (!result.length) {
+      Item.insertMany(defaultItems).then(console.log("Inserted item values"));
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        title: "Today",
+        newListItems: result,
+      });
+    }
+  });
+});
+
+app.get("/:customListName", (req, res) => {
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({ name: customListName }).then((result) => {
+    if (!result) {
+      const customList = new List({
+        name: customListName,
+        items: defaultItems,
+      });
+      customList.save();
+      res.redirect("/" + customListName);
+    } else {
+      res.render("list", {
+        title: result.name,
+        newListItems: result.items,
+      });
+    }
   });
 });
 
 app.post("/", (req, res) => {
   // ADD New Item
-  item = req.body.newItem;
-  if (item) {
-    items.push(item);
-  }
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
 
-  // DELETE button
-  if (req.body.delete) {
-    const index = items.indexOf(req.body.delete);
-    if (index > -1) {
-      // only splice array when item is found
-      items.splice(index, 1);
-    }
-  }
+  const newItem = new Item({ name: itemName });
 
-  res.redirect("/");
+  if (listName == "Today") {
+    newItem.save();
+    res.redirect("/");
+  } else {
+    List.findOne({ name: listName }).then((result) => {
+      result.items.push(newItem);
+      result.save();
+      res.redirect("/" + listName);
+    });
+  }
 });
 
-app.listen(3000, () => {
-  console.log(`Flying on port 3000`);
+app.post("/delete", (req, res) => {
+  const Id = req.body.checkbox;
+  const list = req.body.list;
+
+  if (list === "Today") {
+    Item.findByIdAndRemove({ _id: Id }).then(res.redirect("/"));
+  } else {
+    List.findOneAndUpdate(
+      { name: list },
+      { $pull: { items: { _id: Id } } }
+    ).then(res.redirect("/" + list));
+  }
 });
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to Database");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
